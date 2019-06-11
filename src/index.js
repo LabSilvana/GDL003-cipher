d6a84536cfd60f2a461b03407df6aa193e893b71
Silvana <silvana15_91@hotmail.com>
2019-06-11 17:53:47 -0500

    Se elimina carpeta node_modules de commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log("hola!!!")
+function cipher() {
+	let input_text = document.getElementById("mensaje_codificado").value;
+	let codeCipher='';
+		for(let i=0; i < input_text.length;i++){
+      let code_ascii = input_text.charCodeAt(i);
+			if(code_ascii >= 65 && code_ascii <= 90){
+					let value_letter = (code_ascii-65 +33)%26 + 65;
+					let capital_letter= String.fromCharCode(value_letter);
+					codeCipher += capital_letter;
+			}
+			else if (code_ascii>=97 && code_ascii<=122) {
+				let value_lower = (code_ascii-97 +33)%26 +97;
+				let letter_lower = String.fromCharCode(value_lower);
+				codeCipher += letter_lower;
+			}
+			else if(code_ascii == 32){
+				let space = " ";
+        codeCipher += space;
+			}
+		}
+		return document.write("Tu mensaje cifrado es: " + codeCipher);
+}
+cipher();
+
+function decipher(){
+	let input_text2 = document.getElementById("mensaje_decodificado").value;
+	let codeDecipher ='';
+		for(let i=0; i < input_text2.length;i++){
+			let Code_ascii = input_text2.charCodeAt(i);
+				if(Code_ascii>=65 && Code_ascii<=90){
+					let value_letter = (Code_ascii+65-33)%26+65 ;
+					let capital_letter= String.fromCharCode(value_letter);
+			codeDecipher+=capital_letter;
+	}
+	else if (Code_ascii>=97 && Code_ascii<=122) {
+		let value_lower =  (Code_ascii+97-45)%26+97;
+		let letter_lower = String.fromCharCode(value_lower);
+		codeDecipher+=letter_lower;
+	}
+	else if(Code_ascii==32){
+		let space = " ";
+    codeDecipher+=space;
+	}
+}
+return document.write("Tu mensaje decodificado es: " + codeDecipher);
+}
+decipher();
