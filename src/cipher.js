d3c23a95f65da5680ad4caa46c77b75430f31e45
Silvana <silvana15_91@hotmail.com>
2019-06-11 22:15:07 -0500

    se modifico el cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
 window.cipher = {
-  // ...
-};
+  encode: function(recorrido, texto) {
+    let codeCipher='';
+    for(let i=0; i < texto.length;i++) {
+      let code_ascii = texto.charCodeAt(i);
+      if(code_ascii >= 65 && code_ascii <= 90) {
+      let value_letter = (code_ascii-65 + recorrido)%26 + 65;
+         let capital_letter= String.fromCharCode(value_letter);
+         codeCipher += capital_letter;
+      }
+      else if (code_ascii>=97 && code_ascii<=122) {
+        let value_lower = (code_ascii-97 + recorrido)%26 +97;
+        let letter_lower = String.fromCharCode(value_lower);
+        codeCipher += letter_lower;
+      }
+      else if(code_ascii == 32){
+        let space = " ";
+        codeCipher += space;
+      }
+    }
+    return codeCipher;
+  },
+  decode: function() {
 
-            
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
-let boton_cipher = document.getElementById('Codificar');
-boton_cipher.addEventListener('click',cipher);
 
-function cipher() {
-	let input_text = document.getElementById("mensaje_codificado").value;
-	let codeCipher='';
-		for(let i=0; i < input_text.length;i++){
-      let code_ascii = input_text.charCodeAt(i);
-			if(code_ascii >= 65 && code_ascii <= 90){
-					let value_letter = (code_ascii-65 + 33)%26 + 65;
-					let capital_letter= String.fromCharCode(value_letter);
-					codeCipher += capital_letter;
-			}
-			else if (code_ascii>=97 && code_ascii<=122) {
-				let value_lower = (code_ascii-97 + 33)%26 +97;
-				let letter_lower = String.fromCharCode(value_lower);
-				codeCipher += letter_lower;
-			}
-			else if(code_ascii == 32){
-				let space = " ";
-        codeCipher += space;
-			}
-		}
-		document.getElementById('resultado').innerHTML = codeCipher;
-}
-cipher();
+let boton_cipher = document.getElementById('Codificar');
+let resultado_cipher = document.getElementById('resultado');
+let input_text = document.getElementById('mensaje_codificado');
+
+boton_cipher.addEventListener('click',()=> {
+	resultado_cipher.innerHTML = window.cipher.encode(33,input_text.value);
+});
 
 function decipher(){
 	let input_text2 = document.getElementById("mensaje_decodificado").value;
