e4423cea71bd8e657580937b1bc74b3650488e22
Silvana <silvana15_91@hotmail.com>
2019-06-11 21:08:01 -0500

    Agrego event listener

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
+let boton_cipher = document.getElementById('Codificar');
+boton_cipher.addEventListener('click',cipher);
+
 function cipher() {
 	let input_text = document.getElementById("mensaje_codificado").value;
 	let codeCipher='';
 		for(let i=0; i < input_text.length;i++){
       let code_ascii = input_text.charCodeAt(i);
 			if(code_ascii >= 65 && code_ascii <= 90){
-					let value_letter = (code_ascii-65 +33)%26 + 65;
+					let value_letter = (code_ascii-65 + 33)%26 + 65;
 					let capital_letter= String.fromCharCode(value_letter);
 					codeCipher += capital_letter;
 			}
 			else if (code_ascii>=97 && code_ascii<=122) {
-				let value_lower = (code_ascii-97 +33)%26 +97;
+				let value_lower = (code_ascii-97 + 33)%26 +97;
 				let letter_lower = String.fromCharCode(value_lower);
 				codeCipher += letter_lower;
 			}
@@ -18,7 +21,7 @@ function cipher() {
         codeCipher += space;
 			}
 		}
-		return document.write("Tu mensaje cifrado es: " + codeCipher);
+		document.getElementById('resultado').innerHTML = codeCipher;
 }
 cipher();
 
@@ -42,6 +45,6 @@ function decipher(){
     codeDecipher+=space;
 	}
 }
-return document.write("Tu mensaje decodificado es: " + codeDecipher);
+ document.getElementById('resultado').innerHTML = codeDecipher;
 }
 decipher();
